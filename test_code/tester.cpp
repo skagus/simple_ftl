4c38470683c9e9041b238f033ba7a4b2c1640a09
Skagus Yun <skagus@gmail.com>
2022-01-14 22:25:16 +0900

    sim engine tester.

---
+++ test_code/tester.cpp
@@ -0,0 +1,44 @@
+
+#include "sim.h"
+#include "timer.h"
+#include "cpu.h"
+#include "power.h"
+
+void tmr_End(uint32 tag, uint32 result)
+{
+	SIM_Print("TMR Expire: %d\n", SIM_GetTick());
+}
+
+void start_cpu(void* pParam)
+{
+	TMR_Init();
+	TMR_Add(0, 7, tmr_End, true);
+	uint32 nLoop = 5;
+	while (nLoop-- > 0)
+	{
+		SIM_Print("In Cpu loop rest %d\n", nLoop);
+		CPU_TimePass(SIM_USEC(10));
+	}
+	POWER_SwitchOff();
+	END_RUN;
+}
+
+
+int gnAddCheck;
+
+int main()
+{
+	int nAddCheck;
+#ifdef _WIN64
+	printf("Add Chk: %llX, %llX, %llX\n", &gnAddCheck, &nAddCheck, main);
+#else
+	printf("Add Chk: %X, %X, %X\n", &gnAddCheck, &nAddCheck, main);
+#endif
+	CPU_InitSim();
+	TMR_InitSim();
+	POWER_InitSim();
+//	CPU_Add((CpuID)0, start_cpu, nullptr);
+	CPU_Add((CpuID)1, start_cpu, nullptr);
+	SIM_Run();
+	return 0;
+}
